c18e8c3c0f915ddf37907f47fd7cf4f86e955d53
Edgar Aldana <arnoldo201@hotmail.com>
2020-11-09 12:08:30 -0600

    Se agrego conexion a la base de datos

--- Server Node/app/controllers/caso.js
+++ Server Node/app/controllers/caso.js
@@ -1,4 +1,7 @@
 const db = require("../models");
+var redisScan = require('redisscan');
+var redis = require('redis');
+var r = redis.createClient('redis://3.21.97.128:80');
 
 // Obtener todos los productos
 exports.getAll = (_req, res) => {
@@ -16,8 +19,7 @@ exports.getAll = (_req, res) => {
     });
 };
 
-
-exports.getAll2 = (_req, res) => {
+exports.getAllMongo = (_req, res) => {
     db.mongoose.connection.db.collection("caso", function(err, collection){
         collection.find({}).toArray(function(err, data){
             if (err){
@@ -30,4 +32,23 @@ exports.getAll2 = (_req, res) => {
                 .send({ message: "datos devueltos", data: data });
         })
     });
+};
+
+exports.getRedis= (_req, res) => {
+    /*r.keys('*', (err, keys) => {
+        console.log(keys)
+      });*/
+    r.lpop('caso', (err, val) => {
+        if (err) { 
+            return res
+            .status(400)
+            .send({ message: "Error obteniendo datos." });
+        }
+        r.lpush('caso',val);
+        return res
+            .status(200)
+            .send({ message: "Error obteniendo datos.", data: val });
+      })
+      
+     
 };
--- Server Node/app/routes/caso.js
+++ Server Node/app/routes/caso.js
@@ -4,7 +4,10 @@ module.exports = app => {
     var router = require("express").Router();
 
     // Devuelve todos los casos
-    router.get("/getAllMongo", caso.getAll);
+    router.get("/getAllMongo", caso.getAllMongo);
+
+    // Devuelve el ultimo valor agregado
+    router.get("/getRedis", caso.getRedis);
 
     // La ruta de la api de productos sera url/productos
     app.use("/", router);
